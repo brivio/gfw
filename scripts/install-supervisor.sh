b617adf053299e1d1a118be332bef8da71c5dd74
brivio wang <brivio@qq.com>
2020-04-03 20:16:42 +0800

    update

---
+++ scripts/install-supervisor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+pip install supervisor
+supervisord
+cat >./supervisord.service <<eof
+[Unit] 
+Description=Supervisor daemon
+
+[Service] 
+Type=forking 
+ExecStart=/usr/bin/supervisord -c /etc/supervisord.conf 
+ExecStop=/usr/bin/supervisorctl shutdown 
+ExecReload=/usr/bin/supervisorctl reload 
+KillMode=process 
+Restart=on-failure 
+RestartSec=42s
+
+[Install] 
+WantedBy=multi-user.target
+eof
+mv ./supervisord.service /usr/lib/systemd/system/
+systemctl enable supervisord
