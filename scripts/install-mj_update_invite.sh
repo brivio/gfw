67fc4228aa1299b7d4bb10f4d440f669a361561a
brivio wang <brivio@qq.com>
2020-04-08 12:15:10 +0800

    update

---
+++ scripts/install-mj_update_invite.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+web_dir=$1
+service_name=mj_update_invite
+service_script=/opt/$service_name.sh
+cmd="php $web_dir/index.php api/task/update_invite"
+
+if [[ ! -r $web_dir/index.php ]];then
+    echo "网站目录不存在"
+    exit
+fi
+
+cat >$service_script <<eof
+#!/bin/bash
+_start()
+{
+    if [[ \$(ps aux|grep "$cmd"|grep -v grep|wc -l) != "1" ]];then
+        $cmd
+    fi
+}
+
+_stop(){
+    for pid in \$(ps axo pid,cmd |grep "$cmd"|grep -v grep|awk '{printf "%s\n",\$1}')
+    do
+        kill \$pid
+    done
+}
+
+if [[ \$1 = 'start' ]];then
+    _start
+elif [[ \$1 = 'stop' ]];then
+    _stop
+elif [[ \$1 = 'restart' ]];then
+    _start
+    _stop
+fi
+eof
+chmod -R 777 $service_script
+
+cat >/usr/lib/systemd/system/$service_name.service <<eof
+[Unit]
+Description=$service_name daemon
+After=network.target
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=5
+User=root
+WorkingDirectory=/root
+ExecStart=$service_script start
+ExecReload=$service_script restart
+ExecStop=$service_script stop
+ 
+[Install]
+WantedBy=multi-user.target
+eof
+
+# systemctl start dms
+# systemctl status dms
+systemctl enable $service_name
